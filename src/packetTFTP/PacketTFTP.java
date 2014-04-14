a9cb357e3084e7609a06b7e666d9935a913dae29
benoitvuillemin <benoitvuillemin@10.0.0.8>
2014-04-14 18:39:25 +0200

    Essai dans createDatagram

--- src/packetTFTP/PacketTFTP.java
+++ src/packetTFTP/PacketTFTP.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package packetTFTP;
 
 import java.io.UnsupportedEncodingException;
@@ -13,16 +12,16 @@ import java.io.UnsupportedEncodingException;
  * @author Dimitri
  */
 public abstract class PacketTFTP {
+
     protected byte[] datagram;
     protected byte[] dataByte;
     protected byte[] opcode;
-    
-    public PacketTFTP (String _opcode){
-        opcode=_opcode;
+
+    public PacketTFTP(String _opcode) {
+        opcode = _opcode.getBytes();
     }
-    
-    public PacketTFTP(byte[] dataStr, byte[] opcode)
-    {
+
+    public PacketTFTP(byte[] dataStr, byte[] opcode) {
         this.dataByte = dataStr;
         this.opcode = opcode;
     }
@@ -30,7 +29,7 @@ public abstract class PacketTFTP {
     public byte[] getDatagram() {
         return datagram;
     }
-       
+
     public byte[] getDataStr() {
         return this.dataByte;
     }
@@ -38,23 +37,25 @@ public abstract class PacketTFTP {
     public byte[] getOpcode() {
         return opcode;
     }
-    
-    public void createDatagram (){
-        String _datagram;
-        try{
+
+    public void createDatagram() {
+        //String _datagram;
+        //try {
             buildDataStr();
-            _datagram=opcode+dataByte;
-            datagram=_datagram.getBytes("ascii");
-        }
-        catch (UnsupportedEncodingException ex) {
+            datagram = new byte[opcode.length + dataByte.length];
+            System.arraycopy(opcode, 0, datagram, 0, opcode.length);
+            System.arraycopy(dataByte, 0, datagram, dataByte.length, dataByte.length);
+            //_datagram = opcode + dataByte;
+            //datagram = _datagram.getBytes("ascii");
+        /*} catch (UnsupportedEncodingException ex) {
             System.err.print("Impossible de créer le packet TFTP");
-        }
-        
+        }*/
+
     }
-    
-    public abstract boolean isDatagramPacket (byte[] datagram);
-    
-    public abstract boolean getDatagramPacket (byte[] _data);
-    
+
+    public abstract boolean isDatagramPacket(byte[] datagram);
+
+    public abstract boolean getDatagramPacket(byte[] _data);
+
     public abstract void buildDataStr();
 }
